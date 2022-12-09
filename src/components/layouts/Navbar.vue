4a2b33f57832e87d222b37c94d901302f5458b11
afifdgr <afifdgr3@gmail.com>
2022-12-09 19:03:20 +0700

    # Changing Navbar Color On Scroll

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,10 +1,11 @@
 <template>
 
-    <nav id="navbar" class="px-4 py-4 sm:py-6 bg-white shadow sm:bg-transparent sm:shadow-none sm:fixed w-full z-10">
+    <nav id="navbar" class="px-4 py-4 sm:py-6 bg-white shadow sm:fixed w-full z-10 transition-colors duration-500"
+        :class="{ 'sm:bg-transparent sm:shadow-none': !showWhiteBackground }">
         <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between">
             <div class="w-full flex flex-row items-center justify-between">
                 <div class="text-3xl font-bold">
-                    <span class="sm:text-white">Afv</span>
+                    <span :class="{ 'sm:text-white': !showWhiteBackground }">Afv</span>
                     <span class="text-primary">Blog</span>
                 </div>
 
@@ -26,8 +27,8 @@
                     </button>
                 </div>
             </div>
-            <div class="mt-4 sm:mt-0 w-full sm:space-x-4 sm:text-right sm:block sm:text-white"
-                :class="{ 'hidden': !isVisible }">
+            <div class="mt-4 sm:mt-0 w-full sm:space-x-4 sm:text-right sm:block"
+                :class="{ 'hidden': !isVisible, 'sm:text-white': !showWhiteBackground }">
                 <NavbarLink to="/articles" label="Articles" />
                 <NavbarLink to="/about" label="About" />
                 <NavbarLink to="/contact" label="Contact" />
@@ -39,7 +40,8 @@
 
 <script>
 import NavbarLink from './NavbarLink.vue';
-import { useToggle } from "@/composables/useToggle"
+import { useToggle } from "@/composables/useToggle";
+import { ref } from "vue";
 
 export default {
     components: {
@@ -50,10 +52,22 @@ export default {
 
     setup() {
         let { isVisible, toggle } = useToggle();
+        let showWhiteBackground = ref(false);
+
+        document.addEventListener("scroll", function () {
+            let bodyTopPosition = document.body.getBoundingClientRect().top;
+
+            if (bodyTopPosition < -150) {
+                showWhiteBackground.value = true;
+            } else {
+                showWhiteBackground.value = false;
+            }
+        })
 
         return {
             isVisible,
-            toggle
+            toggle,
+            showWhiteBackground
         }
     }
 
--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,7 +1,66 @@
 <template>
 
   <div>
+
     <Hero />
+
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
+        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
+        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
+        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
+        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
+        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
+        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
+      </p>
+    </div>
+
   </div>
 
 </template>
