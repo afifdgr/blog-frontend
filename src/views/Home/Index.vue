9790d4873590c0cd490559e7eef1d2887c5ef3bd
afifdgr <afifdgr3@gmail.com>
2022-12-09 21:03:00 +0700

    # Designing Articles List on Home Page

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -4,61 +4,8 @@
 
     <Hero />
 
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat hic fugiat dicta facere animi quae temporibus
-        optio, officia blanditiis perspiciatis quam iste modi quia rem? Adipisci necessitatibus inventore nisi sint ut
-        minima, suscipit sequi alias beatae odio. Sint quisquam corporis ut mollitia quod voluptate consequuntur
-        excepturi adipisci similique necessitatibus ducimus veritatis iure quam et non, cumque culpa iusto enim
-        consectetur? Minus sit voluptates odio voluptatem voluptas, obcaecati itaque voluptate nam quod vitae, eius
-        laudantium autem. Expedita repudiandae adipisci maiores labore temporibus voluptatibus quasi exercitationem
-        optio? Nam, vero ab voluptatum veritatis delectus eveniet culpa quod quae illo quaerat amet maiores beatae autem
-      </p>
+    <div class="py-8 sm:py-20 px-4 sm:px-0 container mx-auto">
+      <Articles />
     </div>
 
   </div>
@@ -67,12 +14,14 @@
 
 <script>
 import Hero from "./Hero.vue";
+import Articles from "@/components/Articles.vue"
 
 export default {
   name: 'Home',
 
   components: {
     Hero,
+    Articles
   }
 
 }
