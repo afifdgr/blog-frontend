d375a8eadef98cbb54b7f6b52b9e34a5e0be5d95
afifdgr <afifdgr3@gmail.com>
2022-12-10 08:38:52 +0700

    # Designing About Us Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home/Index.vue";
+import About from "../views/About/Index.vue";
 
 const routes = [
   {
@@ -10,11 +11,7 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
---
+++ src/views/About/Index.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div class="relative z-0 h-40 sm:h-64">
+        <img src="/images/image-2.jpg" alt="about" class="h-full w-full object-cover">
+
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div class="absolute inset-0 bg-gradient-to-t from-primary opacity-50"></div>
+
+        <div class="absolute inset-0">
+            <div
+                class="container mx-auto px-4 sm:px-0 text-white h-full w-full flex flex-col items-center justify-center sm:justify-end text-2xl sm:text-5xl">
+                <div class="sm:mb-8 text-center">
+                    About Us
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mx-auto px-4 sm:px-0 py-8 sm:py-12">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptate cupiditate architecto iste
+            necessitatibus
+            deleniti nisi dignissimos enim qui placeat incidunt, odit aut reiciendis omnis suscipit nam possimus, cumque
+            quod tempore quia pariatur quas fuga sequi id! Excepturi et dolores eligendi deserunt odit dignissimos, in
+            magnam, quia ea, ipsum illum!
+        </p>
+        <p class="mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptate cupiditate architecto iste
+            necessitatibus
+            deleniti nisi dignissimos enim qui placeat incidunt, odit aut reiciendis omnis suscipit nam possimus, cumque
+            quod tempore quia pariatur quas fuga sequi id! Excepturi et dolores eligendi deserunt odit dignissimos, in
+            magnam, quia ea, ipsum illum!
+        </p>
+        <p class="mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptate cupiditate architecto iste
+            necessitatibus
+            deleniti nisi dignissimos enim qui placeat incidunt, odit aut reiciendis omnis suscipit nam possimus, cumque
+            quod tempore quia pariatur quas fuga sequi id! Excepturi et dolores eligendi deserunt odit dignissimos, in
+            magnam, quia ea, ipsum illum!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
